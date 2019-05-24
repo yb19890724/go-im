407c2f6df51f790bed0f96b8c111b2b9351d0a1c
yb19890724 <354037968@qq.com>
2019-05-24 11:40:40 +0800

    添加静态资源

---
+++ webs/im/src/components/auth/login.vue
@@ -0,0 +1,128 @@
+<template>
+    <div>
+        <h1>im登录</h1>
+        <div class="mui-content" id="pageapp">
+            <form id='login-form' class="mui-input-group">
+                <div class="mui-input-row">
+                    <label>账号</label>
+                    <input v-model="user.mobile" placeholder="请输入手机号" type="text" class="mui-input-clear mui-input" >
+                </div>
+                <div class="mui-input-row">
+                    <label>密码</label>
+                    <input v-model="user.passwd" placeholder="请输入密码"  type="password" class="mui-input-clear mui-input" >
+                </div>
+            </form>
+            <div class="mui-content-padded">
+                <button @click="login"  type="button"  class="mui-btn mui-btn-block mui-btn-primary">登录</button>
+                <div class="link-area"><a id='reg' href="register.shtml">注册账号</a> <span class="spliter">|</span> <a  id='forgetPassword'>忘记密码</a>
+                </div>
+            </div>
+            <div class="mui-content-padded oauth-area">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function(){
+            return {
+                user:{
+                    mobile:"",
+                    passwd:""
+                }
+            }
+        },
+        methods:{
+            login:function(){
+                this.$http.post("http://localhost:9090").then((response) => {
+                    console.log("Suc")
+                }).catch(({response}) => {
+                    console.log("Err")
+                });
+            },
+        }
+
+    }
+</script>
+
+<style scoped>
+    @import "../../assets/css/mui.min.css";
+
+    .area {
+        margin: 20px auto 0px auto;
+    }
+
+    .mui-input-group {
+        margin-top: 10px;
+    }
+
+    .mui-input-group:first-child {
+        margin-top: 20px;
+    }
+
+    .mui-input-group label {
+        width: 22%;
+    }
+
+    .mui-input-row label~input,
+    .mui-input-row label~select,
+    .mui-input-row label~textarea {
+        width: 78%;
+    }
+
+    .mui-checkbox input[type=checkbox],
+    .mui-radio input[type=radio] {
+        top: 6px;
+    }
+
+    .mui-content-padded {
+        margin-top: 25px;
+    }
+
+    .mui-btn {
+        padding: 10px;
+    }
+
+    .link-area {
+        display: block;
+        margin-top: 25px;
+        text-align: center;
+    }
+
+    .spliter {
+        color: #bbb;
+        padding: 0px 8px;
+    }
+
+    .oauth-area {
+        position: absolute;
+        bottom: 20px;
+        left: 0px;
+        text-align: center;
+        width: 100%;
+        padding: 0px;
+        margin: 0px;
+    }
+
+    .oauth-area .oauth-btn {
+        display: inline-block;
+        width: 50px;
+        height: 50px;
+        background-size: 30px 30px;
+        background-position: center center;
+        background-repeat: no-repeat;
+        margin: 0px 20px;
+        /*-webkit-filter: grayscale(100%); */
+        border: solid 1px #ddd;
+        border-radius: 25px;
+    }
+
+    .oauth-area .oauth-btn:active {
+        border: solid 1px #aaa;
+    }
+
+    .oauth-area .oauth-btn.disabled {
+        background-color: #ddd;
+    }
+</style>
---
+++ webs/im/src/helps/http.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+/**
+ * Create Axios
+ */
+export const http = axios.create({
+    baseURL: "localhost:9090"
+});
+
+
+/**
+ * Handle all error messages.
+ */
+http.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    const { response } = error;
+
+    if ([401].indexOf(response.status) >= 0) {
+        if (response.status == 401 && response.data.error.message != 'Unauthorized') {
+            return Promise.reject(response);
+        }
+        window.location = '/login';
+    }
+
+    if([422].indexOf(response.status) >= 0){
+        let message = response.message;
+        for(var i in response.data.errors){
+            message = response.data.errors[i][0];
+        }
+        alert(message);
+    }
+
+    return Promise.reject(error);
+});
+
+export default function install(Vue) {
+    Object.defineProperty(Vue.prototype, '$http', {
+        get() {
+            return http
+        }
+    })
+}
